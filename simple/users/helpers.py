fc0a0f51ebe86408cb8f8e2eb4cbf6769ffb3080
Mohamed Abd-El-Gani <45873305+bismohamed@users.noreply.github.com>
2023-08-12 23:21:46 +0300

    add mail body

--- simple/users/helpers.py
+++ simple/users/helpers.py
@@ -1,20 +1,48 @@
 from functools import wraps
 from flask_login import current_user
 from flask_mail import Message
-from flask import url_for
+from flask import url_for,current_app
 from simple import mail
 
 def send_reset_email(user):
     token = user.get_reset_token()
     
+    # Compose the HTML email content
+    html = f"""
+    <html>
+    <head></head>
+    <body>
+        <div style="background-color: #f2f2f2; padding: 20px; text-align: center;">
+            <img src="cid:logo" alt="Your Logo" style="width: 200px;">
+            <h1>Simple Data School Password Reset</h1>
+            <p>To reset your password, click the following link:</p>
+            <a href="{url_for('users.reset_password', token=token, _external=True)}">Reset Password</a>
+            <p>If you did not request this reset, please ignore this email.</p>
+        </div>
+    </body>
+    </html>
+    """
+    
+    # Create a Message object with HTML content
     msg = Message(
         "Simple Data School Password Reset",
         sender="mohamed.pay.878@gmail.com",
-        recipients=[user.email],
-        body=f"""To Reset your password, Visit the following Link
-                {url_for('users.reset_password', token = token,_external=True)}
-                If you did not make this, please ignore this Email""",
+        recipients=[user.email]
     )
+    
+    msg.html = html
+    
+    # Attach your logo as an inline image
+    logo_path = 'static/PngItem_1822974.png'  # Relative path to your image
+    with current_app.open_resource(logo_path) as logo:
+        msg.attach(
+            "logo.png",
+            "image/png",
+            logo.read(),
+            "inline",
+            headers=[["Content-ID", "<logo>"]]
+        )
+
     mail.send(msg)
 
 
